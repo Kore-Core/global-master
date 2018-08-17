475c5d62606d11e041b5cbd539f62d10a346ab14
Lico <lico@coreblockchain>
2018-08-17 14:17:09 -0700

    adding dependencies

---
+++ depends/packages/packages.mk
@@ -0,0 +1,20 @@
+packages:=boost openssl
+darwin_packages:=zeromq
+linux_packages:=zeromq
+native_packages := native_ccache native_comparisontool
+
+qt_native_packages = native_protobuf
+qt_packages = qrencode protobuf
+
+qt_linux_packages= 
+qt_darwin_packages=
+qt_mingw32_packages=
+
+
+wallet_packages=bdb
+
+upnp_packages=miniupnpc
+
+ifneq ($(build_os),darwin)
+darwin_native_packages=native_cctools native_cdrkit native_libdmg-hfsplus
+endif
